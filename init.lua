ee6f5ada60187aae3675514682d5f9a06825f92d
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-02-11 01:23:50 +0200

    Add formatter

--- init.lua
+++ init.lua
@@ -1,103 +1,143 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " "
 
-
 require("lazy").setup("plugins")
 
 --- color theme ---
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
 
-vim.api.nvim_set_keymap('n', '<leader>ff', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", "<leader>ff", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
 
 --- option + w to quickly circle around panes ---
-vim.api.nvim_set_keymap('n', '∑', '<C-w>w', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "∑", "<C-w>w", { noremap = true, silent = true })
 
 local function tree_on_attach(bufnr)
-  local api = require "nvim-tree.api"
+	local api = require("nvim-tree.api")
 
-  local function opts(desc)
-    return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true}
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
 	end
 
-  api.config.mappings.default_on_attach(bufnr)
+	api.config.mappings.default_on_attach(bufnr)
 
-  vim.keymap.set('n', '<C-p>', api.tree.change_root_to_parent,        opts('Up'))
-  vim.keymap.set('n', '?',     api.tree.toggle_help,                  opts('Help'))
+	vim.keymap.set("n", "<C-p>", api.tree.change_root_to_parent, opts("Up"))
+	vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
 end
 
+--- file explorer ---
+
 local HEIGHT_RATIO = 0.8 --
-local WIDTH_RATIO = 0.5  --
+local WIDTH_RATIO = 0.5 --
 
-require("nvim-tree").setup {
-  on_attach = tree_on_attach,
-  disable_netrw = true,
-  hijack_netrw = true,
-  respect_buf_cwd = true,
-  sync_root_with_cwd = true,
-  view = {
-    relativenumber = true,
-    float = {
-      enable = true,
-      open_win_config = function()
-        local screen_w = vim.opt.columns:get()
-        local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
-        local window_w = screen_w * WIDTH_RATIO
-        local window_h = screen_h * HEIGHT_RATIO
-        local window_w_int = math.floor(window_w)
-        local window_h_int = math.floor(window_h)
-        local center_x = (screen_w - window_w) / 2
-        local center_y = ((vim.opt.lines:get() - window_h) / 2)
-                         - vim.opt.cmdheight:get()
-        return {
-          border = "rounded",
-          relative = "editor",
-          row = center_y,
-          col = center_x,
-          width = window_w_int,
-          height = window_h_int,
-        }
-        end,
-    },
-    width = function()
-      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
-    end,
-  },
-}
+require("nvim-tree").setup({
+	on_attach = tree_on_attach,
+	disable_netrw = true,
+	hijack_netrw = true,
+	respect_buf_cwd = true,
+	sync_root_with_cwd = true,
+	view = {
+		relativenumber = true,
+		float = {
+			enable = true,
+			open_win_config = function()
+				local screen_w = vim.opt.columns:get()
+				local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+				local window_w = screen_w * WIDTH_RATIO
+				local window_h = screen_h * HEIGHT_RATIO
+				local window_w_int = math.floor(window_w)
+				local window_h_int = math.floor(window_h)
+				local center_x = (screen_w - window_w) / 2
+				local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+				return {
+					border = "rounded",
+					relative = "editor",
+					row = center_y,
+					col = center_x,
+					width = window_w_int,
+					height = window_h_int,
+				}
+			end,
+		},
+		width = function()
+			return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+		end,
+	},
+})
 
-vim.keymap.set("n", "<leader>s",
-  "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
+--- fuzzy finder & live grep ---
 
-vim.keymap.set("n", "<leader>g",
-  "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>s", "<cmd>lua require('fzf-lua').files()<CR>", { silent = true })
 
-vim.diagnostic.config({ virtual_text = { spacing = 0 }, update_in_insert = false, })
+vim.keymap.set("n", "<leader>g", "<cmd>lua require('fzf-lua').live_grep()<CR>", { silent = true })
+
+vim.diagnostic.config({ virtual_text = { spacing = 0 }, update_in_insert = false })
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes:1"
 
-vim.keymap.set("n", "<leader>h",
-  "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { silent = true })
-vim.keymap.set("n", "<leader>m",
-  "<cmd>lua require('harpoon.mark').add_file()<CR>", { silent = true })
+--- tabs ---
 
-vim.keymap.set("n", "<leader>1",
-  "1gt<CR>", { silent = true })
-vim.keymap.set("n", "<leader>2",
-  "2gt<CR>", { silent = true })
-vim.keymap.set("n", "<leader>3",
-  "3gt<CR>", { silent = true })
-vim.keymap.set("n", "<leader>4",
-  "4gt<CR>", { silent = true })
+vim.keymap.set("n", "<leader>h", "<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>m", "<cmd>lua require('harpoon.mark').add_file()<CR>", { silent = true })
 
+vim.keymap.set("n", "<leader>1", "1gt<CR>", { silent = true })
+vim.keymap.set("n", "<leader>2", "2gt<CR>", { silent = true })
+vim.keymap.set("n", "<leader>3", "3gt<CR>", { silent = true })
+vim.keymap.set("n", "<leader>4", "4gt<CR>", { silent = true })
+
+--- formatting ---
+
+-- Utilities for creating configurations
+local util = require("formatter.util")
+
+require("formatter").setup({
+	logging = true,
+	log_level = vim.log.levels.WARN,
+	filetype = {
+		lua = {
+			require("formatter.filetypes.lua").stylua,
+
+			function()
+				if util.get_current_buffer_file_name() == "special.lua" then
+					return nil
+				end
+
+				return {
+					exe = "stylua",
+					args = {
+						"--search-parent-directories",
+						"--stdin-filepath",
+						util.escape_path(util.get_current_buffer_file_path()),
+						"--",
+						"-",
+					},
+					stdin = true,
+				}
+			end,
+		},
+
+		["*"] = {
+			require("formatter.filetypes.any").remove_trailing_whitespace,
+		},
+	},
+})
+
+--- format on save ---
+local augroup = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+augroup("__formatter__", { clear = true })
+autocmd("BufWritePost", {
+	group = "__formatter__",
+	command = ":FormatWrite",
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,23 +1,20 @@
 return {
 	{
-	  "windwp/nvim-autopairs",
-	  dependencies = { 'hrsh7th/nvim-cmp' },
-	  config = function()
-	    require("nvim-autopairs").setup {}
-	    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-	    local cmp = require('cmp')
-	    cmp.event:on(
-	      'confirm_done',
-	      cmp_autopairs.on_confirm_done()
-	    )
-	  end,
-	}, 
-	{ 
-		"catppuccin/nvim", 
-		name = "catppuccin", 
-		priority = 1000, 
-		config = function() 
-			require('catppuccin').setup({
+		"windwp/nvim-autopairs",
+		dependencies = { "hrsh7th/nvim-cmp" },
+		config = function()
+			require("nvim-autopairs").setup({})
+			local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+			local cmp = require("cmp")
+			cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+		end,
+	},
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		priority = 1000,
+		config = function()
+			require("catppuccin").setup({
 				term_colors = true,
 				transparent_background = true,
 				color_overrides = {
@@ -33,48 +30,50 @@ return {
 						style = "nvchad",
 					},
 				},
-			}) 
-		end 
+			})
+		end,
 	},
-{
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-    },
+	{
+		"nvim-tree/nvim-tree.lua",
+		version = "*",
+		lazy = false,
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 	{
 		"prettier/vim-prettier",
-		build = 'yarn install --frozen-lockfile'
+		build = "yarn install --frozen-lockfile",
 	},
 	{
-  "ibhagwan/fzf-lua",
-  -- optional for icon support
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    -- calling `setup` is optional for customization
-    local actions = require "fzf-lua.actions"
+		"ibhagwan/fzf-lua",
+		-- optional for icon support
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			-- calling `setup` is optional for customization
+			local actions = require("fzf-lua.actions")
 
-    require("fzf-lua").setup({
-	actions = {
-		files = {
-			["ctrl-t"]     = actions.file_edit_or_qf,
-			["ctrl-s"]      = actions.file_split,
-			["ctrl-v"]      = actions.file_vsplit,
-			["default"]      = actions.file_tabedit,
-			["alt-q"]       = actions.file_sel_to_qf,
-			["alt-l"]       = actions.file_sel_to_ll,
-    		}
-	 }
-    })
-  end
-},
-{
-	"ThePrimeagen/harpoon"
-},
-{
-	"nvim-lua/plenary.nvim"
-}
-
+			require("fzf-lua").setup({
+				actions = {
+					files = {
+						["ctrl-t"] = actions.file_edit_or_qf,
+						["ctrl-s"] = actions.file_split,
+						["ctrl-v"] = actions.file_vsplit,
+						["default"] = actions.file_tabedit,
+						["alt-q"] = actions.file_sel_to_qf,
+						["alt-l"] = actions.file_sel_to_ll,
+					},
+				},
+			})
+		end,
+	},
+	{
+		"ThePrimeagen/harpoon",
+	},
+	{
+		"nvim-lua/plenary.nvim",
+	},
+	{
+		"mhartington/formatter.nvim",
+	},
 }
