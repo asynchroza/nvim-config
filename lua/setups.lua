680b65a3c664bf442cc2c5b882a40f16a93f5493
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-02-11 10:07:16 +0200

    Fix typescript formatting

--- lua/setups.lua
+++ lua/setups.lua
@@ -51,37 +51,18 @@ require("nvim-tree").setup({
 })
 
 -- formatting ---
-
--- Utilities for creating configurations
-local util = require("formatter.util")
-
 require("formatter").setup({
 	logging = true,
 	log_level = vim.log.levels.WARN,
 	filetype = {
+		typescript = {
+			require("formatter.filetypes.typescript").prettier,
+		},
+		javascript = {
+			require("formatter.filetypes.javascript").prettier,
+		},
 		lua = {
 			require("formatter.filetypes.lua").stylua,
-
-			function()
-				if util.get_current_buffer_file_name() == "special.lua" then
-					return nil
-				end
-
-				return {
-					exe = "stylua",
-					args = {
-						"--search-parent-directories",
-						"--stdin-filepath",
-						util.escape_path(util.get_current_buffer_file_path()),
-						"--",
-						"-",
-					},
-					stdin = true,
-				}
-			end,
-		},
-		typescript = {
-			require("formatter.filetypes.typescript"),
 		},
 		["*"] = {
 			require("formatter.filetypes.any").remove_trailing_whitespace,
