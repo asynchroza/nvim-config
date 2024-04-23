dac71dc9e67fcac02d3f1204dd42996636eeb982
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-04-23 22:02:17 +0300

    Clean up

--- lua/before.lua
+++
@@ -1,6 +0,0 @@
---- add stuff you want to load before lazy here ---
-
-vim.g.mapleader = " "
-vim.api.nvim_set_option("mouse", "a")
-vim.api.nvim_set_option("clipboard", "unnamedplus")
-vim.api.nvim_set_option("mousemodel", "extend")
