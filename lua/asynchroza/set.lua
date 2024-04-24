0b0a5d9d46df3ca3e267b10bd7508c80b3cec860
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-04-25 01:03:12 +0300

    Add neotree

--- lua/asynchroza/set.lua
+++ lua/asynchroza/set.lua
@@ -7,5 +7,3 @@ vim.api.nvim_set_option("mousemodel", "extend")
 vim.diagnostic.config({ virtual_text = { spacing = 0 }, update_in_insert = false }) --- stops diagnostics from moving text around
 vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes:1"
-
-
