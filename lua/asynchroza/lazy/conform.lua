f19b0c334fe9317e48f54a04033a63f6b62c9f9f
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-09-23 12:35:18 +0200

    Fix trouble

--- lua/asynchroza/lazy/conform.lua
+++ lua/asynchroza/lazy/conform.lua
@@ -1,21 +1,21 @@
 --- formatter ---
 
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				javascript = { { "prettierd" } },
-				typescript = { { "prettierd" } },
-				javascriptreact = { { "prettierd" } },
-				typescriptreact = { { "prettierd" } },
-				lua = { "stylua" },
-				markdown = { "prettier" },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-			},
-		})
-	end,
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                javascript = { { "prettierd" } },
+                typescript = { { "prettierd" } },
+                javascriptreact = { { "prettierd" } },
+                typescriptreact = { { "prettierd" } },
+                lua = { "stylua" },
+                markdown = { "prettier" },
+            },
+            format_on_save = {
+                lsp_fallback = true,
+            },
+        })
+    end,
 }
