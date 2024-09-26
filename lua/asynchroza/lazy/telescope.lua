daaf10482999d34fbe0391c212675445bf9bb883
Michael Bozhilov <michaelbozhilov@gmail.com>
2024-09-26 12:24:29 +0300

    Add keymaps

--- lua/asynchroza/lazy/telescope.lua
+++ lua/asynchroza/lazy/telescope.lua
@@ -16,22 +16,17 @@ return {
 		})
 
 		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>f", builtin.find_files, {})
-		vim.keymap.set("n", "<leader>gt", builtin.help_tags, {})
-		vim.keymap.set("n", "<leader>s", function()
-			builtin.live_grep()
-		end)
+		vim.keymap.set("n", "<leader>f", builtin.find_files, {
+            desc = "Telescope: Find Files, Search for files in the current directory"
+        })
+		vim.keymap.set("n", "<leader>gt", builtin.help_tags, { desc = "Telescope: Help Tags" })
+		vim.keymap.set("n", "<leader>s", builtin.live_grep, { desc = "Telescope: Live Grep. Search for stuff in files" })
 		vim.keymap.set("n", "<leader>rr", function()
 			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		vim.keymap.set("n", "<leader>gb", function()
-			builtin.git_branches()
-		end)
-		vim.keymap.set("n", "<leader>gc", function()
-			builtin.git_commits()
-		end)
-		vim.keymap.set("n", "<leader>gs", function()
-			builtin.treesitter()
-		end)
+		end, { desc = "Telescope: Grep String, Search for string before showing telescope"})
+		vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Telescope: Show git branches" })
+		vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "Telescope: Show git commits" })
+		vim.keymap.set("n", "<leader>gs", builtin.treesitter, { desc = "Telescope: Treesitter, Symbols" })
+        vim.keymap.set("n", "<leader>k", builtin.keymaps, { desc = "Telescope: Keymaps, Shortcuts, Commands" })
 	end,
 }
