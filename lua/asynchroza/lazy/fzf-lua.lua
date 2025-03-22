bd09c6eb1df11a3cdb0290865cd1f61008df3dd6
Michael Bozhilov <michaelbozhilov@gmail.com>
2025-03-22 10:45:05 +0200

    Add comment about case insensitive searching flag

--- lua/asynchroza/lazy/fzf-lua.lua
+++ lua/asynchroza/lazy/fzf-lua.lua
@@ -1,5 +1,6 @@
 return {
   "ibhagwan/fzf-lua",
   dependencies = { "nvim-tree/nvim-web-devicons" },
+  -- fzf has a flag for case insensitive searching if smart case continues to behave poorly
   opts = {}
 }
