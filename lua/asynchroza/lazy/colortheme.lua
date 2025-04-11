aa2fa1502d7c4d1f0a945aac05007c991c19c0d2
Michael Bozhilov <michaelbozhilov@gmail.com>
2025-04-12 00:43:45 +0300

    OXIDE

--- lua/asynchroza/lazy/colortheme.lua
+++ lua/asynchroza/lazy/colortheme.lua
@@ -1,24 +1,24 @@
-local minimalCustom = {
-  background = '#121212',
-  primary = '#ff0088',
-  secondary = '#d5d5d5',
-  noir_0 = '#ffffff',
-  noir_1 = '#f5f5f5',
-  noir_2 = '#d5d5d5',
-  noir_3 = '#b4b4b4',
-  noir_4 = '#a7a7a7',
-  noir_5 = '#949494',
-  noir_6 = '#737373',
-  noir_7 = '#535353',
-  noir_8 = '#323232',
-  noir_9 = '#212121',
-  diagnostic_error = '#ff0038',
-  diagnostic_warning = '#ff7700',
-  diagnostic_info = '#47eae0',
-  diagnostic_hint = '#47eae0',
-  diff_add = '#00ff77',
-  diff_change = '#47eae0',
-  diff_delete = '#ff0038',
+local OXIDE = {
+  background = "#070e11",
+  primary = "#00d992",
+  secondary = "#E9F5E9",
+  noir_0 = "#ffffff",
+  noir_1 = "#f5f5f5",
+  noir_2 = "#d5d5d5",
+  noir_3 = "#b4b4b4",
+  noir_4 = "#a7a7a7",
+  noir_5 = "#949494",
+  noir_6 = "#737373",
+  noir_7 = "#3A4649",
+  noir_8 = "#1D2526",
+  noir_9 = "#10181B",
+  diagnostic_error = "#fa5e86",
+  diagnostic_warning = "#ffb717",
+  diagnostic_info = "#00d992",
+  diagnostic_hint = "#00d992",
+  diff_add = "#00d992",
+  diff_change = "#90a7f8",
+  diff_delete = "#fa5e86",
 }
 
 return {
@@ -29,7 +29,7 @@ return {
   lazy = false,
   priority = 1000,
   opts = {
-    colors = minimalCustom
+    colors = OXIDE
   },
 }
 
